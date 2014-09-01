00384df9ab448a6bae0af345d8cb66324e0b84ba
NKUCodingCat <chenjingtao930@gmail.com>
2014-09-01 21:43:56 +0800

    20140901dev

--- RSS-for-NKU-SMS/client.py
+++ RSS-for-NKU-SMS/client.py
@@ -1,6 +1,22 @@
 ﻿#coding = utf-8
 import requests
-Ver = 0
-url = "file:///"
 
-requests.get(url+"C:/Version")
+Ver = 0
+url = "https://raw.githubusercontent.com/NKUCodingCat/RSS-for-NKU/master/RSS-for-NKU-SMS/"
+xml_set=[
+		'kydt.xml',
+		'xwzx.xml',
+		'bksjx.xml',
+		'yjsjx.xml',
+		'xsgz.xml',
+		'zsxx.xml'
+	]
+def update ():
+	for i in xml_set:
+		requests.get(url+i)
+if __name__ == '__main__':
+	VerNow = int(requests.get(url+"Version").content)
+	if VerNow > Ver:
+		update()
+	else:
+		pass
