5176bd9e159c1ae5f04d2121cda22a5f176de375
NKUCodingCat <chenjingtao930@gmail.com>
2014-09-01 21:56:39 +0800

    20140901dev

--- RSS-for-NKU-SMS/client.py
+++ RSS-for-NKU-SMS/client.py
@@ -1,6 +1,6 @@
 ﻿#coding = utf-8
 import requests
-
+import time
 Ver = 0
 url = "https://raw.githubusercontent.com/NKUCodingCat/RSS-for-NKU/master/RSS-for-NKU-SMS/"
 xml_set=[
@@ -15,8 +15,13 @@ def update ():
 	for i in xml_set:
 		requests.get(url+i)
 if __name__ == '__main__':
-	VerNow = int(requests.get(url+"Version").content)
-	if VerNow > Ver:
-		update()
-	else:
-		pass
+	while 1:
+		time.sleep(0.5)
+		if not int(time.time())%60:
+
+			VerNow = int(requests.get(url+"Version").content)
+			if VerNow > Ver:
+				update()
+
+			else:
+				pass
